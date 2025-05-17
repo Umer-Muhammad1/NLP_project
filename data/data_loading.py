f8125fb5b7dd9d01bbe99a81252dc19261b49361
Umer-7856 <umerteachs22@gmail.com>
2025-05-17 22:13:06 +0200

    resolving cahe_dir issue in data_loading

--- data/data_loading.py
+++ data/data_loading.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 
 def load_training_data():
-    dataset = load_dataset("tatsu-lab/alpaca")
+    dataset = load_dataset("tatsu-lab/alpaca" , cache_dir="/content/.cache/hf_datasets")
     print(f"Dataset loaded with {len(dataset['train'])} training examples")
     return dataset
